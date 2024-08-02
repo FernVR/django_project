b53aa60d3e18efbcad01044a2d3c3f02fac84695
Fern Van Rooyen <fern.w.1998@gmail.com>
2024-08-02 12:51:11 +0000

    modify files for deployment

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0_e*=xi$kze=zsi!4tat5i+k$o&&^5v=(gm)%n_f--ap!en5i#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-fernvr-djangoproject-91yf7cxa2de.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-fernvr-djangoproject-91yf7cxa2de.ws.codeinstitute-ide.net','.herokuapp.com']
 
 
 # Application definition
